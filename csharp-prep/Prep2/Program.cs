b3b6567239a16995ee08042c2150d590b781da4b
David Akindileni <davidakindileni@yahoo.co.uk>
2023-10-26 10:06:48 +0100

    complete core1

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,32 @@
 using System;
+using System.Runtime.InteropServices.Marshalling;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please input your grade percentage: ");
+        string grade = Console.ReadLine();
+        int intGrade = int.Parse(grade);
+
+        if (intGrade >= 90)
+        {
+            Console.WriteLine($"Your letter grade is A");
+        }   else if (intGrade >= 80)
+            {
+                Console.WriteLine($"Your letter grade is B");
+            }   else if (intGrade >= 70)
+                {
+                    Console.WriteLine($"Your letter grade is C");
+                }   else if (intGrade >= 60)
+                    {
+                        Console.WriteLine($"Your letter grade is D");
+                    }
+        else
+        {
+            Console.WriteLine($"Your letter grade is F");
+        }
+
+        
     }
 }
