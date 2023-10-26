07efefea5341ab9e7dd33eedf7d2fa98a7eb5c4d
David Akindileni <davidakindileni@yahoo.co.uk>
2023-10-26 14:09:49 +0100

    complete prep3 core1

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? : ");
+        string magicNum = Console.ReadLine();
+        int magicNumber = int.Parse(magicNum);
+        Console.Write("What is the guess number? : ");
+        string guessNum = Console.ReadLine();
+        int guessNumber = int.Parse(guessNum);
+        if (guessNumber > magicNumber)
+        {
+            Console.WriteLine("Go lower");
+        } else if (guessNumber < magicNumber)
+            {
+                Console.WriteLine("Go higher");
+            }
+                else if (guessNumber == magicNumber)
+                {
+                    Console.WriteLine("You guess it right!");
+                }
     }
 }
