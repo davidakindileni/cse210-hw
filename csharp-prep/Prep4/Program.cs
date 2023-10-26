2361b085ab21804e0916ab61dec13d9cfec1d337
David Akindileni <davidakindileni@yahoo.co.uk>
2023-10-26 16:38:06 +0100

    complete prep4 core1

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        string userInput ="";
+        List<int> myList;
+        myList = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        do
+        {
+            Console.Write("Enter number: ");
+            userInput = Console.ReadLine();
+            if (userInput != "0")
+                {
+                    int userInt = int.Parse(userInput);
+                    myList.Add(userInt);
+                }
+        } while (userInput != "0");
+
+        int listSum = myList.Sum();
+        Console.WriteLine($"The sum of the list is {listSum}");
     }
 }
