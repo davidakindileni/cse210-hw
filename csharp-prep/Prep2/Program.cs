21495db32d6e6042f4563c3fceb9f3c50e47e93a
David Akindileni <davidakindileni@yahoo.co.uk>
2023-10-26 10:14:06 +0100

    complete Core2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,27 +5,40 @@ class Program
 {
     static void Main(string[] args)
     {
+        // request input of the grade percentage
         Console.Write("Please input your grade percentage: ");
         string grade = Console.ReadLine();
+
+        // convert the input to an integer
         int intGrade = int.Parse(grade);
 
+        // iterate to determine the letter grade
         if (intGrade >= 90)
         {
-            Console.WriteLine($"Your letter grade is A");
+            Console.WriteLine("Your letter grade is A");
         }   else if (intGrade >= 80)
             {
-                Console.WriteLine($"Your letter grade is B");
+                Console.WriteLine("Your letter grade is B");
             }   else if (intGrade >= 70)
                 {
-                    Console.WriteLine($"Your letter grade is C");
+                    Console.WriteLine("Your letter grade is C");
                 }   else if (intGrade >= 60)
                     {
-                        Console.WriteLine($"Your letter grade is D");
+                        Console.WriteLine("Your letter grade is D");
                     }
         else
         {
-            Console.WriteLine($"Your letter grade is F");
+            Console.WriteLine("Your letter grade is F");
         }
+
+        // determine if the grade percentage passed or not and display appropriate message
+        if (intGrade >= 70)
+        {
+            Console.WriteLine("Congratulations! You have passed!");
+        }   else 
+            {
+                Console.WriteLine("Don't give up. Please try again. You've got this!");
+            }
 
         
     }
