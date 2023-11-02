66ac58ea2dd5fcdb09169a78059e27ccb4e00754
David Akindileni <davidakindileni@yahoo.co.uk>
2023-11-02 19:20:03 +0100

    complete abstraction learning activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // create job1
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2015;
+        job1._endYear = 2021;
+
+        // create job2
+        Job job2 = new Job();
+        job2._jobTitle = "FrontEnd Web Developer";
+        job2._company = "Meta";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        // create resume
+        Resume resume1 = new Resume();
+        resume1._name = "James Baldwin";
+
+        // add the jobs
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        // display the resume with job details
+        resume1.DisplayResumeDetails();
+        
     }
 }
