7d465fc95960ebc386d927a3ce414fb521114fc9
David Akindileni <davidakindileni@yahoo.co.uk>
2023-11-10 09:50:44 +0100

    W03 learning03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fract1 = new Fraction();
+        Console.WriteLine(fract1.GetFractionAsStr());
+        Console.WriteLine(fract1.GetFractionAsDecimalVal());
+
+        Fraction fract2 = new Fraction(2);
+        Console.WriteLine(fract2.GetFractionAsStr());
+        Console.WriteLine(fract2.GetFractionAsDecimalVal());
+
+        Fraction fract3 = new Fraction(7, 11);
+        Console.WriteLine(fract3.GetFractionAsStr());
+        Console.WriteLine(fract3.GetFractionAsDecimalVal());
+
+        Fraction fract4 = new Fraction(21, 56);
+        Console.WriteLine(fract4.GetFractionAsStr());
+        Console.WriteLine(fract4.GetFractionAsDecimalVal());
     }
 }
