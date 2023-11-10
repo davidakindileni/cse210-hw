40fdb63239c2adc9f78db14c5208652094f9f127
David Akindileni <davidakindileni@yahoo.co.uk>
2023-11-10 09:06:36 +0100

    amend journal.cs

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -6,7 +6,7 @@ using System.Text;
 public class Jrnl
 {
     public List<JrnlEntry> _jrnl = new List<JrnlEntry> ();
-    private string userFileName;
+    private string _userFileName;
 
     // public Jrnl()
     // {
@@ -44,32 +44,32 @@ public class Jrnl
         {
             Console.Write("\n\tEnter filename to save to: ");
             string userInput = Console.ReadLine();
-            userFileName = userInput + ".txt";
+            _userFileName = userInput + ".txt";
 
-            if (File.Exists(userFileName))
+            if (File.Exists(_userFileName))
             {
                 // Append journal entries
-                using (StreamWriter outputFile = new StreamWriter(userFileName, append: true))
+                using (StreamWriter outputFile = new StreamWriter(_userFileName, append: true))
                 {
                     foreach (JrnlEntry jrnlEntry in _jrnl)
                     {
                         outputFile.WriteLine($"{jrnlEntry._dateTime}; {jrnlEntry._jrnlPrompt}; {jrnlEntry._jrnlEntry}");
                     }
                 }
-                Console.Write($"\tJournal entries have been successfully added to the {userFileName}.\n");
+                Console.Write($"\tJournal entries have been successfully added to the {_userFileName}.\n");
                 // reset journal file
                 _jrnl.Clear();
             }
             else
             {
                 Console.ForegroundColor=ConsoleColor.Red;
-                Console.Write($"\t{userFileName} does not exist. Create the file and save the entries (y or n)? ");
+                Console.Write($"\t{_userFileName} does not exist. Create the file and save the entries (y or n)? ");
                 Console.ForegroundColor=ConsoleColor.White;
                 string userResp = Console.ReadLine();
 
                 if ((userResp == "y") || (userResp == "Y"))
                 {
-                    using (StreamWriter outputFile = new StreamWriter(userFileName))
+                    using (StreamWriter outputFile = new StreamWriter(_userFileName))
                     {
                         foreach (JrnlEntry jrnlEntry in _jrnl)
                         {
@@ -77,7 +77,7 @@ public class Jrnl
                         }
                     }
 
-                    Console.Write($"\n\tCreated {userFileName} successfully.\n");
+                    Console.Write($"\n\tCreated {_userFileName} successfully.\n");
                     Console.Write("\tJournal entries recorded.\n");
                 // reset journal file
                 _jrnl.Clear();
@@ -90,12 +90,12 @@ public class Jrnl
     {
         Console.Write("\n\tEnter the filename to load: ");
         string userInput = Console.ReadLine();
-        userFileName = userInput + ".txt";
-        if (File.Exists(userFileName))
+        _userFileName = userInput + ".txt";
+        if (File.Exists(_userFileName))
         {
-            if (userFileName.Length > 0)
+            if (_userFileName.Length > 0)
             {
-                List<string> readText = File.ReadAllLines(userFileName).Where(arg => !string.IsNullOrWhiteSpace(arg)).ToList();
+                List<string> readText = File.ReadAllLines(_userFileName).Where(arg => !string.IsNullOrWhiteSpace(arg)).ToList();
                 foreach (string line in readText)
                 {
                     string[] entries = line.Split("; ");
@@ -106,22 +106,22 @@ public class Jrnl
                     entry._jrnlEntry = entries[2];
                     _jrnl.Add(entry);
                 }
-                Console.Write($"\tJournal entries from {userFileName} have been successfully loaded.\n");
+                Console.Write($"\tJournal entries from {_userFileName} have been successfully loaded.\n");
                 // empty loaded file
                 string sourceFile="blank.txt";
-                File.Copy(sourceFile, userFileName, true);
+                File.Copy(sourceFile, _userFileName, true);
             }
             else
             {
                 Console.ForegroundColor=ConsoleColor.Red;
-                Console.Write($"\n\t{userFileName} is empty. Please try again.\n");
+                Console.Write($"\n\t{_userFileName} is empty. Please try again.\n");
                 Console.ForegroundColor=ConsoleColor.White;
             }
         }
         else
         {
             Console.ForegroundColor=ConsoleColor.Red;
-            Console.Write($"\n\t{userFileName} does not exist. Please try again.\n");
+            Console.Write($"\n\t{_userFileName} does not exist. Please try again.\n");
             Console.ForegroundColor=ConsoleColor.White;
         }
     }
