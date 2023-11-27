dbc45c1135c868435e98d56b74e0c42a94015567
David Akindileni <davidakindileni@yahoo.co.uk>
2023-11-27 21:00:21 +0100

    complete W04 learning activity

---
+++ prepare/Learning04/MathAssignment.cs
@@ -0,0 +1,45 @@
+using System;
+
+public class MathAssignment : Assignment
+{
+    // define the member variables
+    private string _textBookSection;
+    private string _problems;
+
+    // create the MathAssignment constructors
+    public MathAssignment()
+    {
+
+    }
+
+    public MathAssignment(string stuName, string topic, string textBookSection, string problems)
+        : base(stuName, topic)
+    {
+        _textBookSection = textBookSection;
+        _problems = problems;
+    }
+
+    // create the getters and setters
+    public string GetTextBookSection()
+    {
+        return _textBookSection;
+    }
+    public void SetTextBookSection(string textBookSection)
+    {
+        _textBookSection = textBookSection;
+    }
+    public string GetProblems()
+    {
+        return _problems;
+    }
+    public void SetProblems(string problems)
+    {
+        _problems = problems;
+    }
+
+    // define the function
+    public string GetHomeworkList()
+    {
+        return $"Section: {_textBookSection} Problems: {_problems}";
+    }
+}
---
+++ prepare/Learning04/WritingAssignment.cs
@@ -0,0 +1,37 @@
+using System;
+
+public class WritingAssignment : Assignment
+{
+    // define the member variables
+    private string _title;
+
+    // create the MathAssignment constructors
+    public WritingAssignment()
+    {
+
+    }
+
+    public WritingAssignment(string stuName, string topic, string title)
+        : base(stuName, topic)
+    {
+        _title = title;
+    }
+
+    // create the getters and setters
+    public string GetTitle()
+    {
+        return _title;
+    }
+
+    public void SetTitle(string title)
+    {
+        _title = title;
+    }
+
+    // define the function
+    public string GetWritingInfo()
+    {
+        string stuName = GetStudentName();
+        return $"{_title} by {stuName}";
+    }
+}
