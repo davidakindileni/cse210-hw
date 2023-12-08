9d43e0bf5e749545fb93402f66a54f67d757aa86
David Akindileni <davidakindileni@yahoo.co.uk>
2023-12-08 21:30:46 +0100

    complete Foundation 4

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Activity> activitiesList = new List<Activity>();
+
+        Running running = new Running(15, 4);
+        activitiesList.Add(running);
+        Cycling cycling = new Cycling(25, 12);
+        activitiesList.Add(cycling);
+        Swimming swimming = new Swimming(40, 70);
+        activitiesList.Add(swimming);
+
+
+        //display activity summaries
+        foreach (Activity activity in activitiesList)
+        {
+            activity.DisplaySummary();
+        }
     }
 }
