7f3ccb1dfaa83421a24b7cd41bdf325cdcd432a8
David Akindileni <davidakindileni@yahoo.co.uk>
2023-12-08 19:03:59 +0100

    Foundation 1 add comments

--- final/Foundation1/Comment.cs
+++ final/Foundation1/Comment.cs
@@ -2,15 +2,18 @@ using System;
 
 public class Comment
 {
+    // define member variables
     private string _name;
     private string _text;
 
+    // define constructor
     public Comment(string name, string text)
     {
         this._name = name;
         this._text = text;
     }
 
+    // define getters and setters
     public string GetComment()
     {
         return $"{this._name}:\n    {this._text}";
